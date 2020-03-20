0ae57cca5956a8344e7fe053504b1306a64c87f9
jj <jjesty@mail.ru>
2020-03-21 01:11:26 +0700

    getOptimalTransport

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -14,9 +14,9 @@ public class PathFinder {
      * Если список transports равен null, то оптимальеый транспорт тоже равен null.
      */
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
+        if(deliveryTask == null || transports == null) return null;
         Map<RouteType, BigDecimal> routeLengthLink = deliveryTask.getRoutes().stream()
                 .collect(Collectors.toMap(Route::getType, Route::getLength));
-
         Optional<Transport> optionalTransport = transports.stream()
                 .filter(transport -> routeLengthLink.containsKey(transport.getType()))
                 .filter(transport -> transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0)
