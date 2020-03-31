8b58107d4a3224252555765dbfe8c18ec3660474
jj <jjestyy@gmail.com>
2020-03-31 20:13:59 +0700

    initialize

---
+++ unit7/src/main/java/com/github/jjestyy/JavaSIS30/unit7/Unit7Application.java
@@ -0,0 +1,13 @@
+package com.github.jjestyy.JavaSIS30.unit7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit7Application.class, args);
+	}
+
+}
---
+++ unit7/src/test/java/com/github/jjestyy/JavaSIS30/unit7/Unit7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jjestyy.JavaSIS30.unit7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
