01f117f8e6367673b90d76bdc0d0c24640c8d2be
jj <jjestyy@gmail.com>
2020-04-15 15:03:14 +0700

    initialize

---
+++ unit-11/app-server/src/main/java/com/github/jjestyy/JavaSIS320/unit11/Unit11Application.java
@@ -0,0 +1,13 @@
+package com.github.jjestyy.JavaSIS320.unit11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit11Application.class, args);
+	}
+
+}
---
+++ unit-11/app-server/src/test/java/com/github/jjestyy/JavaSIS320/unit11/Unit11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jjestyy.JavaSIS320.unit11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
